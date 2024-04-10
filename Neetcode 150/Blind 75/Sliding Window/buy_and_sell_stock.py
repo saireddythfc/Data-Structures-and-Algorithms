0702b7fb48d946513bf1b8bf25c31bc0ed7b535d
saireddythfc <saivivaswanthreddy@gmail.com>
2024-04-09 19:31:09 -0700

    Sliding window easy

---
+++ Neetcode 150/Blind 75/Sliding Window/buy_and_sell_stock.py
@@ -0,0 +1,63 @@
+"""
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and 
+choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+"""
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        bestProfit = 0
+        maxL = [prices[-1]]
+
+        for i in range(len(prices) - 2, -1, -1):
+            if prices[i] > maxL[-1]:
+                price = prices[i]
+            else:
+                price = maxL[-1]
+            maxL.append(price)
+
+        maxL.reverse()
+
+        for i, stock in enumerate(prices):
+            if i == len(prices) - 1:
+                break
+            if bestProfit < maxL[i + 1] - stock:
+                bestProfit = maxL[i + 1] - stock
+
+        return bestProfit
+
+
+"""
+
+class Solution:
+    def maxProfit(self, prices):
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
+
+"""
